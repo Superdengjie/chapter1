ad38a92940d57ccba464afebfaa4ba22ea7f1f46
itcast <hello@itcast.cn>
2020-03-25 11:50:51 +0800

    Initial commit

---
+++ src/main/java/com/courese/testng/BasicAnnotation.java
@@ -0,0 +1,12 @@
+package com.courese.testng;
+
+
+import org.testng.annotations.Test;
+
+@Test
+public class BasicAnnotation {
+    public void testCase1(){
+        System.out.println("testCase1这是测试用例1");
+    }
+
+}
